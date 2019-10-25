accfea83697ccc9b07c61deaa8c1662de5284eb1
Mario Nishio <manishio@microsoft.com>
2019-10-25 11:57:06 -0700

    added new feature

---
+++ Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTasksProvider
+{
+    public static class Constants
+    {
+        private static readonly string SQLDBConnectionString = "Data Source=test.database.windows.net;Initial Catalog=test;Persist Security Info=True;User ID=test;Password=password123";
+
+        public const string IssuerKey = "dOTCs2xYkd7CWPJfhRvqCg0U1CRdL8PhhfRGGxgd4Ts="; 
+
+        string sasUrl = @"https://test.table.core.windows.net/test?sv=2013-08-15&tn=test&sig=kfEzhzz4JDTnfOaCgau3sfl4EE2QHrWbBE88FKVjubo%3D"; 
+    }
+}
